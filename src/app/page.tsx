e6acba427478bd12cdbf2a5c7cc8c402ccddb28f
juanlopez1 <juan_manuel_lopez_1996@hotmail.com>
2024-10-02 19:49:12 -0300

    add home message

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,21 @@
 import { type FC, Fragment } from 'react';
 
 import Header from '@free-market-web-ui/components/Header';
+import ArrowUp from '@free-market-web-ui/icons/ArrowUp';
 
 const HomePage: FC = () => (
     <Fragment>
         <Header />
-        <main>home</main>
+        <main>
+            <div className="flex flex-col justify-center items-center mt-2">
+                <div className="animate-bounce">
+                    <ArrowUp className="w-6 h-6" />
+                </div>
+                <div className="text-2xl text-[--gray]">
+                    ¡Empezá a buscar ahora y encontrá tu producto en instantes!
+                </div>
+            </div>
+        </main>
     </Fragment>
 );
 
