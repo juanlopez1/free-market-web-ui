b7e4c70dbba8528a219e9f19b5df7b9ecd445d6a
juanlopez1 <juan_manuel_lopez_1996@hotmail.com>
2024-10-04 05:36:37 -0300

    fix footer .env var

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,13 +2,15 @@ import Link from 'next/link';
 
 import pkg from '@free-market-web-ui/../package.json';
 
-const Footer = () => (
-    <footer>
-        <Link href={process.env.GITHUB_REPO_URL as string} target="_blank">
-            GitHub
-        </Link>
-        |<p>Free Market Web UI by Juan Manuel López</p>|<p>v{pkg.version}</p>
-    </footer>
-);
+const Footer = () => {
+    return (
+        <footer>
+            <Link href={process.env.NEXT_PUBLIC_GITHUB_REPO_URL as string} target="_blank">
+                GitHub
+            </Link>
+            |<p>Free Market Web UI by Juan Manuel López</p>|<p>v{pkg.version}</p>
+        </footer>
+    );
+};
 
 export default Footer;
