1f0fd3035a18a4fa1f7e1cf9f82b99ee2444aa9a
juanlopez1 <juan_manuel_lopez_1996@hotmail.com>
2024-10-04 06:42:18 -0300

    add processDefaultErrors for handling erros

--- src/services/http.service.ts
+++ src/services/http.service.ts
@@ -1,3 +1,18 @@
+export const processDefaultErrors = (response: Response) => {
+    if (!response.ok) {
+        if (response.status === 400) {
+            throw new Error(`Bad request: ${response.status} `);
+        }
+        if (response.status === 404) {
+            throw new Error(`Not found: ${response.status} `);
+        }
+        if (response.status >= 500) {
+            throw new Error(`Server error: ${response.status} `);
+        }
+        throw new Error(`Failed to proceed: ${response.status}`);
+    }
+};
+
 export async function httpGet<T>(endpoint: string): Promise<T> {
     const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}${endpoint}`, {
         method: 'GET',
@@ -5,5 +20,6 @@ export async function httpGet<T>(endpoint: string): Promise<T> {
             'Content-Type': 'application/json',
         },
     });
+    processDefaultErrors(response);
     return response.json();
 }
